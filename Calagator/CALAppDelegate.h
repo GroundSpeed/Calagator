d153b7de71394f73d38694eb584ba9be2b56178c
donmiller <dmiller@jadr.biz>
2013-07-05 23:37:51 -0400

    Initial Commit

---
+++ Calagator/CALAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CALAppDelegate.h
+//  Calagator
+//
+//  Created by Don Miller on 7/5/13.
+//  Copyright (c) 2013 GroundSpeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CALAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calagator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calagator
+//
+//  Created by Don Miller on 7/5/13.
+//  Copyright (c) 2013 GroundSpeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CALAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CALAppDelegate class]));
+    }
+}
